9100c54fad97e60d984a2dbc60af060c8d1639fd
chadg30 <78976404+chadg30@users.noreply.github.com>
2021-02-24 11:58:01 -0500

    Add files via upload

---
+++ SCM.py
@@ -0,0 +1,37 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def getusername_passwd():
+    valuser=False
+    valpass=False
+
+    username = input("Enter username: <email address> ")
+    while valuser != True:
+        if "@" in username and ".com" in username:
+            valuser=True
+        else:
+            username = input("Incorrect, try again: ")
+
+    while valpass != True:
+        password = input("Enter your password. Enter at least 8 characters, with at least "
+                         "one letter, one number, and a character from [#,$,%,*]\n")
+        if "#" in password or "$" in password or "%" in password or "*" in password:
+            valpass=True
+
+    return username, password
+
+
+def main():
+    vusername,vpassword=getusername_passwd()
+    print(vpassword)
+    print(vusername)
+
+
+if __name__ == "__main__":
+    main()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
